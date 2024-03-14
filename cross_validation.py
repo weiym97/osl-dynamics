4584166694fba362883f1d323409aebb8d4030fc
weiym97 <weiym97@163.com>
2024-03-14 10:49:14 +0000

    WIP, cross validation

--- cross_validation.py
+++ cross_validation.py
@@ -13,62 +13,83 @@ from osl_dynamics.models import load
 from osl_dynamics.utils.plotting import plot_mode_pairing
 
 if __name__ == '__main__':
-    save_dir= './results_simulation_202403/sigma_0/'
-    N_states = list(range(2,17))
+    save_dir= './results_HCP_cv/'
+    N_states = list(range(2,37))
     index = int(sys.argv[1]) - 1
     N_state = N_states[index]
-    N_splits = 5
-    data_dir = './data/node_timeseries/simulation_202402/sigma_0/'
-    z_score_data = False
+    N_splits = 10
+    data_dir = './data/node_timeseries/3T_HCP1200_MSMAll_d25_ts2/'
+    z_score_data = True
 
-
+    dataset = Data(data_dir)
+    dataset.standardize()
+    dataset.select(timepoints=[0, 1200])
 
     for N_split in range(1, N_splits + 1):
-        analysis_dir = f'{save_dir}hmm_ICA_25_state_{N_state}/split_{N_split}/'
+        analysis_dir = f'{save_dir}hmm_ICA_25_state_{N_state}/cv_{N_split}/'
         print(f'analysis_dir: {analysis_dir}')
         split_1_dir = f'{analysis_dir}half_1/'
         split_2_dir = f'{analysis_dir}half_2/'
-
+        '''
         with open(f'{split_1_dir}inf_params/alp.pkl', 'rb') as file:
             alpha_1 = pickle.load(file)
         with open(f'{split_2_dir}inf_params/alp.pkl', 'rb') as file:
             alpha_2 = pickle.load(file)
-
-        dataset = Data(data_dir)
-        dataset.standardize()
-        dataset.select(timepoints=[0,1200])
+        '''
 
         model_1 = load(f'{split_1_dir}model')
         model_2 = load(f'{split_2_dir}model')
 
-        with open(f'{analysis_dir}indices_1.json') as f:
+        with open(f'{analysis_dir}indices_3.json') as f:
             # Load the JSON data
-            indice_1 = json.load(f)
+            indice_3 = json.load(f)
 
-        with open(f'{analysis_dir}/indices_2.json') as f:
+        with open(f'{analysis_dir}/indices_4.json') as f:
             # Load the JSON data
-            indice_2 = json.load(f)
+            indice_4 = json.load(f)
 
-        with dataset.set_keep(indice_1):
-            alpha_1_cv = model_2.get_alpha(dataset)
+        with dataset.set_keep(indice_3):
+            alpha_3_model_1 = model_1.get_alpha(dataset)
+            alpha_3_model_2 = model_2.get_alpha(dataset)
 
-        with dataset.set_keep(indice_2):
-            alpha_2_cv = model_1.get_alpha(dataset)
+        temp_cor= alpha_correlation(alpha_3_model_1,alpha_3_model_2,return_diagonal=False)
+
+        pair_1_2, temp_cor_1_2_pair = hungarian_pair(temp_cor)
+        pair_2_1, temp_cor_2_1_pair = hungarian_pair(temp_cor.T)
+
+        with open(f'{analysis_dir}pair_1_2.json', "w") as file:
+            json.dump(pair_1_2, file)
+
+        with open(f'{analysis_dir}pair_2_1.json', "w") as file:
+            json.dump(pair_2_1, file)
+
+        np.save(f'{analysis_dir}temp_cor_1_2.npy',temp_cor_1_2_pair)
+        np.save(f'{analysis_dir}temp_cor_2_1.npy',temp_cor_2_1_pair)
 
 
-        temp_cor_1 = alpha_correlation(alpha_1,alpha_1_cv,return_diagonal=False)
-        temp_cor_2 = alpha_correlation(alpha_2,alpha_2_cv,return_diagonal=False)
-
-        pair_1, temp_cor_1_pair = hungarian_pair(temp_cor_1)
-        pair_2, temp_cor_2_pair = hungarian_pair(temp_cor_2)
-
-
-        plot_mode_pairing(temp_cor_1_pair,pair_1,filename=f'{analysis_dir}temp_cor_1.jpg')
-        plot_mode_pairing(temp_cor_2_pair, pair_2, filename=f'{analysis_dir}temp_cor_2.jpg')
+        plot_mode_pairing(temp_cor_1_2_pair,pair_1_2,filename=f'{analysis_dir}temp_cor_1.jpg')
+        plot_mode_pairing(temp_cor_2_1_pair, pair_2_1, filename=f'{analysis_dir}temp_cor_2.jpg')
 
         cov_1 = np.load(f'{split_1_dir}inf_params/covs.npy')
         cov_2 = np.load(f'{split_2_dir}inf_params/covs.npy')
 
+        # Calculate the time series of model 1 on split 4, and then
+        # substitute the covariances with their siblings from model 2
+        # Then calculate the free energy.
+        with dataset.set_keep(indice_4):
+            alpha_4_model_1 = model_1.get_alpha(dataset)
+            model_1.set_covariances(cov_2[pair_1_2['col']])
+            free_energy_cv_1, log_likelihood_cv_1, entropy_cv_1, prior_cv_1 = model_1.free_energy(dataset,return_components=True)
+            evidence_cv_1 = model_1.evidence(dataset)
+
+        # Similar implementation of model 2
+        with dataset.set_keep(indice_4):
+            alpha_4_model_2 = model_2.get_alpha(dataset)
+            model_2.set_covariances(cov_1[pair_2_1['col']])
+            free_energy_cv_2, log_likelihood_cv_2, entropy_cv_2, prior_cv_2 = model_2.free_energy(dataset,return_components=True)
+            evidence_cv_2 = model_2.evidence(dataset)
+
+        '''
         # Set the covariances after the pairing
         model_1.set_covariances(cov_2[pair_1['col']])
         model_2.set_covariances(cov_1[pair_2['col']])
@@ -78,7 +99,7 @@ if __name__ == '__main__':
         with dataset.set_keep(indice_2):
             free_energy_cv_2, log_likelihood_cv_2, entropy_cv_2, prior_cv_2 = model_2.free_energy(dataset,return_components=True)
             evidence_cv_2 = model_2.evidence(dataset)
-
+        '''
         with open(f'{analysis_dir}free_energy_cv_1.json',"w") as file:
             metrics_cv_1 = {'free_energy': float(free_energy_cv_1),
                             'log_likelihood': float(log_likelihood_cv_1),
@@ -96,12 +117,3 @@ if __name__ == '__main__':
                             'evidence': float(evidence_cv_2),
                             }
             json.dump(metrics_cv_2, file)
-
-
-
-
-
-
-
-
-
