cab67ba96552435f2169445f11037bf11b1f176e
weiym97 <weiym97@163.com>
2024-07-12 15:19:11 +0100

    WIP, generate SWC data

--- simulation_main.py
+++ simulation_main.py
@@ -570,7 +570,7 @@ if __name__ == '__main__':
     data = sim.time_series
     time_course = sim.state_time_course
     data = data.reshape(n_subjects, -1, n_channels)
-    time_course = get_one_hot(time_course,n_states=n_states)
+    time_course = get_one_hot(time_course,n_states=n_states).reshape(n_subjects, -1, n_states)
 
     np.save(f'{save_dir}truth/state_covariances.npy', covariances)
     #np.save(f'{save_dir}truth/tpm.npy', sim.hmm.trans_prob)
