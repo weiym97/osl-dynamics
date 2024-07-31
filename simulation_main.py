5df27f6806fcf8a076a0dfabf4062dc48211db30
weiym97 <weiym97@163.com>
2024-07-31 10:54:39 +0100

    WIP, new simulation DyNeMo

--- simulation_main.py
+++ simulation_main.py
@@ -579,6 +579,7 @@ if __name__ == '__main__':
         np.savetxt(f'{save_dir}{10001 + i}.txt', data[i])
         np.save(f'{save_dir}truth/{10001 + i}_state_time_course.npy', time_course[i])
     '''
+    '''
     # Update 29th July 2024
     # Generate DyNeMo style simulation
     save_dir = './data/node_timeseries/simulation_bicv/real_dynemo/'
@@ -619,7 +620,44 @@ if __name__ == '__main__':
     for i in range(n_subjects):
         np.savetxt(f'{save_dir}{10001 + i}.txt', data[i])
         np.save(f'{save_dir}truth/{10001 + i}_mode_time_course.npy', time_course[i])
+    '''
 
+
+
+    # Generate DyNeMo simulation using Chet and Rukuang's code.
+    save_dir = './data/node_timeseries/simulation_bicv/random_dynemo/'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    if not os.path.exists(f'{save_dir}truth/'):
+        os.makedirs(f'{save_dir}truth')
+
+    n_subjects = 500
+    n_modes = 6
+    n_samples = 1200
+    n_channels = 50
+
+    sim = simulation.MixedSine_MVN(
+        n_samples=n_subjects*n_samples,
+        n_modes=n_modes,
+        n_channels=n_channels,
+        relative_activation=[1, 0.5, 0.5, 0.25, 0.25, 0.1],
+        amplitudes=[6, 5, 4, 3, 2, 1],
+        frequencies=[1, 2, 3, 4, 6, 8],
+        sampling_frequency=250,
+        means="zero",
+        covariances="random",
+    )
+
+    data = sim.time_series
+    time_course = sim.mode_time_course
+    data = data.reshape(n_subjects, -1, n_channels)
+    time_course = time_course.reshape(n_subjects, -1, n_modes)
+
+    np.save(f'{save_dir}truth/state_covariances.npy', sim.obs_mod.covariances)
+
+    for i in range(n_subjects):
+        np.savetxt(f'{save_dir}{10001 + i}.txt', data[i])
+        np.save(f'{save_dir}truth/{10001 + i}_mode_time_course.npy', time_course[i])
 
     #############################################################
     '''
