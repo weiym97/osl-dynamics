aaaa3e623dc9af7a628d3cd77ff5e1c72bf53650
weiym97 <weiym97@163.com>
2024-10-01 07:53:43 +0100

    WIP,AR

--- autocorrelation.py
+++ autocorrelation.py
@@ -8,8 +8,11 @@ from osl_dynamics.config_api.wrappers import load_data
 if __name__ == '__main__':
     data_kwargs_path = sys.argv[1]
     with open(data_kwargs_path,'r') as file:
-        data_kwargs = yaml.load(file)
+        data_kwargs = yaml.safe_load(file)
     data = load_data(**data_kwargs)
     print(len(data.time_series()))
     print(data.time_series[0].shape)
     print(np.var(data.time_series[0], axis=0))
+
+    data_time_series = data.time_series()
+
