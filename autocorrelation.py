752a892c7756443236052acf837f2bdc0aae6c46
weiym97 <weiym97@163.com>
2024-10-01 08:42:38 +0100

    WIP, AR

--- autocorrelation.py
+++ autocorrelation.py
@@ -10,7 +10,6 @@ from osl_dynamics.config_api.wrappers import load_data
 def fit_ar1_model(time_series):
     # Fit an AR(1) model to the time series using ARIMA with order (1,0,0)
     model = sm.tsa.ARIMA(time_series, order=(1, 0, 0)).fit()
-    print(model.params)
     return model.params
 
 if __name__ == '__main__':
@@ -20,21 +19,25 @@ if __name__ == '__main__':
         data_kwargs = yaml.safe_load(file)
     data = load_data(**data_kwargs)
     data_time_series = data.time_series()
-    print(len(data_time_series))
-    print(data_time_series[0].shape)
-    print(np.var(data_time_series[0], axis=0))
 
     n_subjects = len(data_time_series)
     n_timepoints, n_channels = data_time_series[0].shape
 
     # To store AR(1) coefficients for each subject and channel
+    ar1_intercepts = np.zeros((n_subjects, n_channels))
     ar1_coefficients = np.zeros((n_subjects, n_channels))
+    ar1_noises = np.zeros((n_subjects,n_channels))
 
     # Iterate through subjects and channels
     for subject in range(n_subjects):
         for channel in range(n_channels):
-            time_series = np.squeeze(data_time_series[0][:,channel])
-            ar1_coefficients[subject, channel] = fit_ar1_model(time_series)[1]  # AR(1) coefficient
-    np.save(save_path,ar1_coefficients)
+            time_series = np.squeeze(data_time_series[subject][:,channel])
+            params = fit_ar1_model(time_series)
+            ar1_intercepts[subject,channel] = params[0]
+            ar1_coefficients[subject, channel] = params[1]  # AR(1) coefficient
+            ar1_noises = params[2]
+    np.save(f'{save_path}/ar1_intercepts.npy',ar1_intercepts)
+    np.save(f'{save_path}/ar1_coefficients.npy', ar1_coefficients)
+    np.save(f'{save_path}/ar1_noises.npy', ar1_noises)
 
 
