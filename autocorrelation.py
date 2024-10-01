e1c93a42434eff2cdc393b73883eec49991b08f3
weiym97 <weiym97@163.com>
2024-10-01 08:07:30 +0100

    WIP, AR

--- autocorrelation.py
+++ autocorrelation.py
@@ -15,17 +15,18 @@ def fit_ar1_model(time_series):
 
 if __name__ == '__main__':
     data_kwargs_path = sys.argv[1]
+    save_path = sys.argv[2]
     with open(data_kwargs_path,'r') as file:
         data_kwargs = yaml.safe_load(file)
     data = load_data(**data_kwargs)
     data_time_series = data.time_series()
     print(len(data_time_series))
-    print(data_time_series()[0].shape)
+    print(data_time_series[0].shape)
     print(np.var(data_time_series[0], axis=0))
-    
+
     n_subjects = len(data_time_series)
     n_timepoints, n_channels = data_time_series[0].shape
-    '''
+
     # To store AR(1) coefficients for each subject and channel
     ar1_coefficients = np.zeros((n_subjects, n_channels))
 
@@ -34,6 +35,6 @@ if __name__ == '__main__':
         for channel in range(n_channels):
             time_series = np.squeeze(data_time_series[0][:,channel])
             ar1_coefficients[subject, channel] = fit_ar1_model(time_series)[0]  # AR(1) coefficient
-    '''
+    np.save(save_path,ar1_coefficients)
 
 
