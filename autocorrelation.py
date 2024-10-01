54b54d797f3ba54fe4502b5c1a237fe04a9fc6e7
weiym97 <weiym97@163.com>
2024-10-01 07:51:11 +0100

    WIP, ar

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,10 +1,15 @@
 import sys
-import pickle
+import yaml
+
+import numpy as np
+
 from osl_dynamics.config_api.wrappers import load_data
+
 if __name__ == '__main__':
     data_kwargs_path = sys.argv[1]
-    with open(data_kwargs_path,'rb') as file:
-        data_kwargs = pickle.load(file)
+    with open(data_kwargs_path,'r') as file:
+        data_kwargs = yaml.load(file)
     data = load_data(**data_kwargs)
     print(len(data.time_series()))
     print(data.time_series[0].shape)
+    print(np.var(data.time_series[0], axis=0))
