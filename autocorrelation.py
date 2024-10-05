b4ebb6342afbbe762bba0d3508b998a5e04aa5e1
weiym97 <weiym97@163.com>
2024-10-05 14:15:24 +0100

    WIP, estimating autocorrelation structure

--- autocorrelation.py
+++ autocorrelation.py
@@ -12,10 +12,11 @@ def fit_ar1_model(time_series):
     model = sm.tsa.ARIMA(time_series, order=(1, 0, 0)).fit()
     return model.params
 
+
 if __name__ == '__main__':
     data_kwargs_path = sys.argv[1]
     save_path = sys.argv[2]
-    with open(data_kwargs_path,'r') as file:
+    with open(data_kwargs_path, 'r') as file:
         data_kwargs = yaml.safe_load(file)
     data = load_data(**data_kwargs)
     data_time_series = data.time_series()
@@ -26,18 +27,16 @@ if __name__ == '__main__':
     # To store AR(1) coefficients for each subject and channel
     ar1_intercepts = np.zeros((n_subjects, n_channels))
     ar1_coefficients = np.zeros((n_subjects, n_channels))
-    ar1_noises = np.zeros((n_subjects,n_channels))
+    ar1_noises = np.zeros((n_subjects, n_channels))
 
     # Iterate through subjects and channels
     for subject in range(n_subjects):
         for channel in range(n_channels):
-            time_series = np.squeeze(data_time_series[subject][:,channel])
+            time_series = np.squeeze(data_time_series[subject][:, channel])
             params = fit_ar1_model(time_series)
-            ar1_intercepts[subject,channel] = params[0]
+            ar1_intercepts[subject, channel] = params[0]
             ar1_coefficients[subject, channel] = params[1]  # AR(1) coefficient
-            ar1_noises = params[2]
-    np.save(f'{save_path}/ar1_intercepts.npy',ar1_intercepts)
+            ar1_noises[subject, channel] = params[2]
+    np.save(f'{save_path}/ar1_intercepts.npy', ar1_intercepts)
     np.save(f'{save_path}/ar1_coefficients.npy', ar1_coefficients)
     np.save(f'{save_path}/ar1_noises.npy', ar1_noises)
-
-
