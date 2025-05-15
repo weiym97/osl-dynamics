b988045ad515d7c08b9db79b6478a5d63e85eeba
weiym97 <weiym97@163.com>
2025-05-15 10:16:38 +0100

    New simulation

--- examples/simulation/evaluation_paper.py
+++ examples/simulation/evaluation_paper.py
@@ -62,6 +62,36 @@ def hmm_iid_final(save_dir, n_subjects, n_samples, n_states, n_channels, tr):
         np.savetxt(f'{save_dir}{10001 + i}.txt', data[i])
         np.save(f'{save_dir}truth/{10001 + i}_state_time_course.npy', time_course[i])
 
+def hmm_iid_mean_only(save_dir, n_subjects, n_samples, n_states, n_channels, tr):
+    save_dir = f'{save_dir}/hmm_iid_mean_only/'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    if not os.path.exists(f'{save_dir}truth/'):
+        os.makedirs(f'{save_dir}truth/')
+
+    covariances = np.array([np.eye(n_channels) for _ in range(n_states)])
+
+    sim = simulation.HMM_MVN(
+        n_samples=n_samples * n_subjects,
+        n_states=n_states,
+        n_channels=n_channels,
+        trans_prob='uniform',
+        stay_prob=0.9,
+        means='random',
+        covariances=covariances
+    )
+    data = sim.time_series
+    time_course = sim.state_time_course
+    data = data.reshape(n_subjects, -1, n_channels)
+    time_course = time_course.reshape(n_subjects, -1, n_states)
+
+    np.save(f'{save_dir}truth/state_covariances.npy', sim.obs_mod.covariances)
+    np.save(f'{save_dir}truth/tpm.npy', sim.hmm.trans_prob)
+
+    for i in range(n_subjects):
+        np.savetxt(f'{save_dir}{10001 + i}.txt', data[i])
+        np.save(f'{save_dir}truth/{10001 + i}_state_time_course.npy', time_course[i])
+
 def hmm_iid_real(save_dir, n_subjects, n_samples, n_states, n_channels, tr):
     import random
     from osl_dynamics.simulation.mvn import MVN
@@ -604,6 +634,8 @@ def main(simulation_list=None):
         hmm_iid(**config)
     if 'hmm_hrf' in simulation_list:
         hmm_hrf(**config)
+    if 'hmm_iid_mean_only' in simulation_list:
+        hmm_iid_mean_only(**config)
     if 'hmm_iid_final' in simulation_list:
         hmm_iid_final(**config)
     if 'hmm_iid_real' in simulation_list:
